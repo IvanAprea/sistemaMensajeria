9b4710237746b04c90c62d14ec909227163f9d25
IvanAprea <54630721+IvanAprea@users.noreply.github.com>
2020-03-26 16:29:55 -0300

    Add files via upload

---
+++ Persona.java
@@ -0,0 +1,42 @@
+package client;
+
+public class Persona {
+    
+    private String IP,Puerto,Nombre = "default",Apellido = "default";
+    
+    public Persona() {
+        super();
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public String getPuerto() {
+        return Puerto;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    public void setPuerto(String Puerto) {
+        this.Puerto = Puerto;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+}
